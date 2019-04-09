b38d5a34d2f4804344a2c33fd40dd70ca0e1ec7c
Jereu <jastudim@outlook.com>
2019-04-09 16:35:46 +0100

    working version

---
+++ src/components/content/crud/CreateTerm.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <h1>Create Term</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "CreateTerm",
+  components: {
+    
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
